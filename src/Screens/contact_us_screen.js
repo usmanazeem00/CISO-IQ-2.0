17320965f33979df2ad67bbe58bf1d0c7f736a73
mhd-usama <sindhuusama8@gmail.com>
2023-11-21 22:04:21 +0500

    complete contact us page

--- src/Screens/contact_us_screen.js
+++ src/Screens/contact_us_screen.js
@@ -1,75 +1,114 @@
 import React from 'react';
 import { Container, Row, Col, NavbarBrand } from 'react-bootstrap';
 import Navbar from './navbar';
+import styled, {ThemeProvider } from "styled-components";
+
+
 const ContactUs = () => {
+    const theme = {
+        colors: {
+          white: '#ffffff',
+          btn: '#add8e6',
+        },
+    };
+    const headingStyle = {
+        color: '#333',
+        fontFamily: 'Arial, sans-serif',
+        textAlign: 'center',
+        textTransform: 'uppercase',
+        letterSpacing: '2px',
+        borderBottom: '2px solid #333',
+        paddingBottom: '5px',
+        marginTop: '20px',
+    };
+
+    const highlightStyle = {
+        backgroundColor: '#000',
+        color: '#fff',
+        padding: '5px',
+      };
+      
+      
+    const Wrapper = styled.section`
+    // padding: 9rem 0 5rem 0;
+    text-align: center;
+
+    .container {
+      margin-top: 6rem;
+
+      .contact-form {
+        max-width: 50rem;
+        margin: auto;
+
+        .contact-inputs {
+          display: flex;
+          flex-direction: column;
+          gap: 3rem;
+
+          input[type="submit"] {
+            cursor: pointer;
+            transition: all 0.2s;
+
+            &:hover {
+                background-color: ${theme.colors.white};
+              border: 1px solid ${theme.colors.btn};
+              color: ${theme.colors.btn};
+              transform: scale(0.9);
+            }
+          }
+        }
+      }
+    }
+  `;
   return (
-    <>
+    <Wrapper>
     <Navbar/>
-    <Container className="mt-5 text-center">
-            <h2 style={{ backgroundColor: 'black', color: 'white', padding: '15px' }}>
-            Our Mission
-        </h2>
-    <Row>
+    <h2 style={headingStyle}>Contact the technical team of <span style={highlightStyle}>Vogue Vista</span></h2>
+    
+    <div style={{ display: 'flex', maxWidth: '1200px', margin: 'auto', marginTop: '20px', marginBottom: '20px' }}>
 
-        <Col md={6}>
-        <img
-            src="/Images/transparent logo.png"
-            alt="Vogue Vista Image"
-            className="img-fluid"
-            style={{ maxWidth: '600px', maxHeight: '300px' }}
-        />
-        </Col>
-        <Col md={6}>
-        <p>
-            Vouge Vista, a distinguished e-commerce platform headquartered in Pakistan, stands as a premier destination for acquiring exquisite clothing and accessories designed for both men and women. With a commitment to delivering quality and a strong focus on customer satisfaction, we prioritize the meticulous craftsmanship of our products. Our extensive range reflects our dedication to utilizing the finest materials and impeccable stitching, ensuring that each item meets the highest standards of excellence. As we set our sights on the future, Vouge Vista envisions a global presence, driven by the same dedication to quality and customer-centric values that define our identity. At the core of our ethos lies an unwavering commitment to providing an unparalleled shopping experience that transcends borders.
-        </p>
-        </Col>
-    </Row>
-      <Row className="mt-4 text-center">
-        <Col>
-        <h2 style={{ backgroundColor: 'black', color: 'white', padding: '15px' }}>
-            Our Mission
-        </h2>
+  <div style={{ flex: 1 }}>
 
-         <p>
-          Our mission at Vouge Vista is to revolutionize the fashion landscape by providing a meticulously curated selection of clothing and accessories that embody sophistication, quality, and individual expression. Dedicated to delivering unparalleled excellence, we prioritize the use of premium materials and precise craftsmanship in every product. As we aspire to extend our influence globally, our unwavering commitment revolves around empowering individuals to embrace their unique style while ensuring the highest standards of customer satisfaction. Vouge Vista envisions not just being a fashion destination but a symbol of authenticity, innovation, and timeless elegance.
-          </p>
-        </Col>
-      </Row>
-      <Row className="mt-4 text-center">
-        <Col>
-        <h2 style={{ backgroundColor: 'black', color: 'white', padding: '15px' }}>
-            Our Team
-        </h2>
-        <div class="card-group">
-        <div class="card">
-            <img class="card-img-top" src="./Images/Zaid.JPG" alt="Card image cap" style={{height:500}}/>
-            <div class="card-body">
-            <h5 class="card-title">Zaid Khan</h5>
-            <p class="card-text">Zaid, our dynamic COO, leads Vogue Vista with a visionary approach, driving innovation and global growth. His commitment to excellence sets the stage for redefining fashion, ensuring unparalleled quality and individual expression.</p>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="./Images/Usama.JPG" alt="Card image cap" style={{height:500}}/>
-            <div class="card-body">
-            <h5 class="card-title">Muhammad Usama</h5>
-            <p class="card-text">As CEO, Usama oversees Vogue Vista's operations with precision, ensuring seamless logistics and a commitment to quality. His leadership enhances the brand's efficiency, delivering an exceptional experience to our customers.</p>
-            </div>
-        </div>
-        <div class="card">
-            <img class="card-img-top" src="./Images/Usman.JPG"  style={{height:500}} alt="Card image cap"/>
-            <div class="card-body">
-            <h5 class="card-title">Muhammad Usman Azeem</h5>
-            <p class="card-text">Usman, our dynamic CMO, elevates Vogue Vista's brand with captivating marketing strategies. He ensures a vibrant market presence and fosters compelling connections with our customers.</p>
-            </div>
-        </div>
-        </div>
-            </Col>
-        </Row>
-        <Row className="mb-5"></Row>
-        </Container>
-        </>
+    <img src="/Images/black.png" alt="Your Logo" style={{ maxWidth: '100%', height: 'auto' }} />
+  </div>
+
+  <div style={{ flex: 1, marginLeft: '20px' }}>
+  <div style={{ backgroundColor: '#f4f4f4', padding: '20px', borderRadius: '8px', boxShadow: '0 0 10px rgba(0, 0, 0, 0.1)' }}>
+        <form action='https://formspree.io/f/xknlaava' method='POST' style={{ display: 'grid', gap: '15px' }}>
+          <div style={{ display: 'grid', gap: '8px' }}>
+            <label htmlFor='username' style={{ fontWeight: 'bold' }}>Username</label>
+            <input type='text' id='username' name='UserName' required autoComplete='off' style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px', boxSizing: 'border-box' }} />
+          </div>
+          <div style={{ display: 'grid', gap: '8px' }}>
+            <label htmlFor='email' style={{ fontWeight: 'bold' }}>EMAIL</label>
+            <input type='email' id='email' name='email' required autoComplete='off' style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px', boxSizing: 'border-box' }} />
+          </div>
+          <div style={{ display: 'grid', gap: '8px' }}>
+            <label htmlFor='message' style={{ fontWeight: 'bold' }}>Message:</label>
+            <textarea id='message' name='message' cols='30' rows='10' required autoComplete='off' placeholder='Enter Your Message' style={{ width: '100%', padding: '8px', border: '1px solid #ccc', borderRadius: '4px', boxSizing: 'border-box' }}></textarea>
+          </div>
+          <div style={{ display: 'grid', gap: '8px' }}>
+            <button type='submit' style={{ backgroundColor: '#4caf50', color: 'white', padding: '10px 15px', border: 'none', borderRadius: '4px', cursor: 'pointer', fontSize: '16px' }}>SEND</button>
+          </div>
+        </form>
+      </div>
+  </div>
+</div>
+<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3402.5515130266126!2d74.30043917457454!3d31.48152117423175!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x391903f08ebc7e8b%3A0x47e934f4cd34790!2sFAST%20NUCES%20Lahore!5e0!3m2!1sen!2s!4v1700580396033!5m2!1sen!2s" 
+    width="100%" 
+    height="450" 
+    style={{border:0}}
+    allowFullScreen="" 
+    loading="lazy" 
+    referrerPolicy="no-referrer-when-downgrade">
+    </iframe>
+
+    
+        </Wrapper>
         );
 };
 
 export default ContactUs;
+
+
+
